8a53b45dbd9e74f7a9ab6ba1be7cce0f69a02cd5
Ignacio <ignacio.henriquez@evalueserve.com>
2023-09-08 11:47:05 -0300

    fix: add css imports to jest config

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
-  },
+    '\\.(css|less)$': '<rootDir>/test/jest/__mocks__/styleMock.js'
+  }
   //   setupFiles: ["./jest.setup.js"],
-};
+}
--- test/App.test.jsx
+++ test/App.test.jsx
@@ -1,9 +1,9 @@
-import { render } from '@testing-library/react';
-import App from '@/App';
+import { render } from '@testing-library/react'
+import App from '@/App'
 
 describe('App Component', () => {
   it('should render the component initially', () => {
-    const { container } = render(<App />);
-    expect(container).toMatchSnapshot();
-  });
-});
+    const { container } = render(<App />)
+    expect(container).toMatchSnapshot()
+  })
+})
