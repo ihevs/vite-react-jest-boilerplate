5831161d9927231edf61c7bfeb543d0904441efe
Ignacio <ignacio.henriquez@evalueserve.com>
2023-09-08 11:49:09 -0300

    fix: add identity obj proxy to jest config

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
-    '\\.(css|less)$': '<rootDir>/test/jest/__mocks__/styleMock.js'
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
   }
   //   setupFiles: ["./jest.setup.js"],
 }
